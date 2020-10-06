70d2d4230f487b0068e1f90bf10b58fa7c37932a
laughingfatman <4WHITT04@solent.ac.uk>
2020-10-06 09:54:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print ("i see you are trying to read a book, what genre of book do you like?")
+genre = input()
+
+print ("you like {}? what a GOAT..." .format(genre))
+
+print ("okay bye for now!")
